7beb952c180bc8a6b557361e617697d1cf9c0486
Chris Robus <christopher.robus@gmail.com>
2019-09-08 18:35:32 -0400

    finished settting up laravel, begining to stub out project

---
+++ addressBook/database/migrations/2019_09_08_221059_create_company_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompanyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('adress');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zip');
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company');
+    }
+}
